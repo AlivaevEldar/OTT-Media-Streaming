5348ef3b91b8230d343854a26f09d0400b27ece1
Алыбаев Элдар <alibaeveldar2010@gmail.com>
2025-08-20 00:04:04 +0300

    настройка react-router-dom

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,18 @@
-import Header from "@widget/Header"
+import Header from "@widget/Header";
+import Home from "@pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App
