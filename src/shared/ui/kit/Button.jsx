7985635ad4378523bb0df95ca6c0e50b17373906
Алыбаев Элдар <alibaeveldar2010@gmail.com>
2025-08-19 11:23:54 +0300

    ui kit iconWrapper и Button

---
+++ src/shared/ui/kit/Button.jsx
@@ -0,0 +1,20 @@
+function Button({ styles = "", children, icon }) {
+  return (
+    <button
+      className={`bg-red-45 text-absolute-white font-semibold flex gap-x-1 items-center justify-center ${styles}`}
+    >
+      {icon ? (
+        <img
+          className="w-6 desktop:w-7"
+          src={icon}
+          alt=""
+          width={24}
+          height={24}
+        />
+      ) : null}
+      {children}
+    </button>
+  );
+}
+
+export default Button;
