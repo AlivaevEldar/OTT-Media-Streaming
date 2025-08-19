25e6e962d10fc7a5769cb2f3766afcf3237b75ce
Алыбаев Элдар <alibaeveldar2010@gmail.com>
2025-08-19 11:08:34 +0300

    alias

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@widget": path.resolve(__dirname, "./src/widget"), 
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    }
+  }
 });
